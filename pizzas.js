5a4a677f7599140eb0d846b76c42069490f5441e
ConstantinoAndre <100153834+ConstantinoAndre@users.noreply.github.com>
2022-06-23 16:20:53 -0300

    teste

---
+++ pizzas.js
@@ -0,0 +1,9 @@
+let pizzaJson = [
+    {id:1, name:'Café tradicional', img:'images/coffee-1.png', price:20.19, sizes:['50ml', '100ml', '150ml'], description:'Descrição: Café tradicional brasileiro'},
+    {id:2, name:'Café com leite', img:'images/café-com-leite1.png', price:18.00, sizes:['50ml', '100ml', '150ml'], description:'Descrição: para todos aqueles que gostam de um delicioso café com leite'},
+    {id:3, name:'Bolo de chocolate', img:'images/bolo-de-chocolate.png', price:17.45, sizes:['320g', '530g', '860g'], description:'Descrição: temos bolo de varios tamanhos , venha experimentar essa delícia'},
+    {id:4, name:'Pão de Queijo', img:'images/pao-de-queijo1.png', price:19.77, sizes:['320g', '530g', '860g'], description:'Descrição: Uma delícia de pão de queijo , todos quentinhos'},
+    {id:5, name:'Pão francês', img:'images/pao-frances.png', price:21.43, sizes:['320g', '530g', '860g'], description:'Descrição: pães fresquinhos'},
+    {id:6, name:'Refrigerantes', img:'images/refrigerante-lata.png', price:18.55, sizes:['320g', '530g', '860g'], description:'Descrição: sabores Coca-cola e Guaraná'},
+    {id:7, name:'Misto quente', img:'images/misto-quente.png', price:22.36, sizes:['320g', '530g', '860g'], description:'Descrição: temos misto quente deliciosos'}
+];
